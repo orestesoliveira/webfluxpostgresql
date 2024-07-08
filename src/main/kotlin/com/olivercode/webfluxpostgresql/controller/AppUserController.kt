f0d273da99349a9dde409b774f37faaa91af3bbf
orestesoliveira <orestesoliveira@gmail.com>
2024-07-08 10:14:12 -0300

    added new controller methods

--- src/main/kotlin/com/olivercode/webfluxpostgresql/controller/AppUserController.kt
+++ src/main/kotlin/com/olivercode/webfluxpostgresql/controller/AppUserController.kt
@@ -1,5 +1,6 @@
 package com.olivercode.webfluxpostgresql.controller
 
+import com.olivercode.webfluxpostgresql.error.NotFoundException
 import com.olivercode.webfluxpostgresql.service.AppUserService
 import com.olivercode.webfluxpostgresql.model.AppUser
 import org.springframework.web.bind.annotation.GetMapping
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("/api")
@@ -19,5 +21,11 @@ class AppUserController (
     }
 
     @GetMapping("/users/{id}")
-    fun getById(@PathVariable id:Long) = appUserService.findAppUserById(id)
+    fun getById(@PathVariable id:Long) =
+        appUserService.findAppUserById(id)
+            .switchIfEmpty(
+                Mono.error(
+                    NotFoundException("User with if $id doenst exist")
+                )
+            )
 }
---
+++ src/main/kotlin/com/olivercode/webfluxpostgresql/error/Error.kt
@@ -0,0 +1,7 @@
+package com.olivercode.webfluxpostgresql.error
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+data class NotFoundException(val msg:String) : RuntimeException(msg)
